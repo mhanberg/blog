ab4920289b84f725efddd1f6b038e6835875f4de
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-10-25 15:05:17 -0400

    script to create to post with default front matter

---
+++ newfile.sh
@@ -0,0 +1,27 @@
+#!/usr/local/bin/zsh
+if [ ! $1 ]
+then
+echo "I need a post name dude"
+exit 1
+fi
+
+POST_NAME=$1
+DATE=`date +%Y-%m-%d`
+TIME="09:00:00 -04:00"
+
+FILE=./_posts/$DATE"-"$POST_NAME".md"
+
+if [ ! -e "$FILE" ]
+then
+cat > $FILE << EOM
+---
+layout: post
+title:
+date: ${DATE} 09:00:00 -04:00
+categories: post
+permalink: /:categories/:year/:month/:day/:title/
+---
+EOM
+else
+echo "File already exists"
+fi
