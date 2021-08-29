6dcd109732cec783ee670c6e42bbc8cc1fbf425f
Mitchell Hanberg <mitch@mitchellhanberg.com>
2021-08-29 19:44:26 -0400

    Actually update tableau/temple

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,3 @@
-const jekyllEnv = process.env.MIX_ENV || "dev";
-
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("autoprefixer"),
-    ...(jekyllEnv != "dev"
-      ? [
-          require("@fullhuman/postcss-purgecss")({
-            content: ["!(_site|node_modules)/**/*.+(html|js|md)", "*.html"],
-            whitelistPatternsChildren: [/highlight/, /mark/, /hll/],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          }),
-          require("cssnano")({ preset: "default" }),
-        ]
-      : [])
-  ]
+  plugins: [require("postcss-import"), require("autoprefixer")],
 };
