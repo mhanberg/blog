531af357852222d2ae4a46619b4799b376eb1bcc
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-07-12 11:45:35 -0400

    fix: broken header links

--- js/index.js
+++ js/index.js
@@ -10,6 +10,11 @@ if (nodes.length != 0) {
   render(nodes, "en_US");
 }
 
+// TODO: remove once mdex updates to a newer comrak that doesnt do this
+document.querySelectorAll("a[inert]").forEach((node) => {
+  node.removeAttribute("inert");
+});
+
 window.Fzf = Fzf;
 window.Alpine = Alpine;
 
